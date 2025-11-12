07de8a36ddcf518e2180423916ec780e35f1ca57
Jesús de la Fuente <jfuentelucas@gmail.com>
2025-11-12 15:02:32 +0100

    first data-structure added

---
+++ src/constants/index.js
@@ -0,0 +1,23 @@
+export const galleryItems = [
+  {
+    title: "Ethereal Awakening",
+    description:
+      "An artistic journey through stone and light. This surreal visual piece explores the boundary between culpture and dream, where texture breathes and stillness evokes emotion.",
+    depthImg: "/images/woman_depth.webp", // mapa de profundidad correspondiente
+    fake3dImg: "/images/woman_fake3d.webp", // tu imagen subida
+  },
+  {
+    title: "Ancient Reverie",
+    description:
+      "A cracked visage frozen in time — this piece captures the fragile beauty of decay, where color and erosion merge into an otherworldly calm.",
+    depthImg: "/images/ancient_depth.webp",
+    fake3dImg: "/images/ancient_fake3d.webp",
+  },
+  {
+    title: "Spirit of the Dragon",
+    description:
+      "A vivid exploration of mythical energy and motion, sculpted in color and texture. The dragon’s roar reverberates through a whirlwind of carved clouds and flowing forms.",
+    depthImg: "/images/dragon_depth.webp",
+    fake3dImg: "/images/dragon_fake3d.webp",
+  },
+];
